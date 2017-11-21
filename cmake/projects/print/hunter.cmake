3e8f9f303f470c9d9408d4b91e214b1669fc37c1
NeverMore27 <uragan.uragan.59@mail.ru>
2017-11-21 23:29:55 +0300

    added stack package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/NeverMore27/st5/archive/v0.1.0.0.tar.gz"
+    SHA1
+    5c07ca5e018b5ddffbb02b584bad232c7b5d31ae
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
